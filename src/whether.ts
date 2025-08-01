752673bc1c4d8fb933f661b60bb55d91205fec63
aldia_mechdream <futami16237@gmail.com>
2025-08-01 13:30:37 +0800

    feat: 增加布尔判定； 添加所有注释

--- src/whether.ts
+++ src/whether.ts
@@ -13,25 +13,57 @@ export class UntypedWhether extends Function {
     return Object.is(a, b);
   }
 
+  /**
+   * Check if the target is **not** one of `undefined`, `null`, `+0`, `-0`, `NaN`, `0`, or `''`
+   * @param o target
+   */
+  isTruthy(o: any): boolean {
+    return Boolean(o);
+  }
+
+  /**
+   * Check if the target is one of `undefined`, `null`, `+0`, `-0`, `NaN`, `0`, or `''`
+   * @param o target
+   */
+  isFalsy(o: any): boolean {
+    return !o;
+  }
+
+  /**
+   * Tell whether the target is negative zero
+   * @param o target
+   */
   isNegativeZero(o: any): boolean {
     return Object.is(o, -0);
   }
 
+  /**
+   * Tell whether the target is positive zero
+   * @param o target
+   */
   isPositiveZero(o: any): boolean {
     return Object.is(o, +0);
   }
 
+  /**
+   * Tell whether the target is a function
+   * @param o target
+   */
   isFunction(o: any): o is Func {
     return typeof o === 'function';
   }
 
+  /**
+   * Tell whether the target is a non-null object
+   * @param o target
+   */
   isObject<T extends object>(o: any): o is T {
     return typeof o === 'object' && o !== null;
   }
 
   /**
    * Tell whether the target is a non-null object or a function
-   * @param o
+   * @param o target
    */
   likeObject(o: unknown) {
     return (typeof o === 'object' && o !== null) || typeof o === 'function';
@@ -39,7 +71,7 @@ export class UntypedWhether extends Function {
 
   /**
    * Tell whether the target is a string
-   * @param o
+   * @param o target
    */
   isString(o: any): o is string {
     return typeof o === 'string';
@@ -47,7 +79,7 @@ export class UntypedWhether extends Function {
 
   /**
    * Tell whether the target is a number
-   * @param o
+   * @param o target
    */
   isNumber(o: any): o is number {
     return typeof o === 'number';
@@ -55,7 +87,7 @@ export class UntypedWhether extends Function {
 
   /**
    * Tell whether the target is a boolean
-   * @param o
+   * @param o target
    */
   isBoolean(o: any): o is boolean {
     return typeof o === 'boolean';
@@ -63,7 +95,7 @@ export class UntypedWhether extends Function {
 
   /**
    * Tell whether the target is undefined
-   * @param o
+   * @param o target
    */
   isUndefined(o: any): o is undefined {
     return typeof o === 'undefined';
@@ -71,7 +103,7 @@ export class UntypedWhether extends Function {
 
   /**
    * Tell whether the target is null
-   * @param o
+   * @param o target
    */
   isNull(o: any): o is null {
     return o === null;
@@ -79,7 +111,7 @@ export class UntypedWhether extends Function {
 
   /**
    * Tell whether the target is a symbol
-   * @param o
+   * @param o target
    */
   isSymbol(o: any): o is symbol {
     return typeof o === 'symbol';
@@ -87,7 +119,7 @@ export class UntypedWhether extends Function {
 
   /**
    * Tell whether the target is a bigint
-   * @param o
+   * @param o target
    */
   isBigInt(o: any): o is bigint {
     return typeof o === 'bigint';
@@ -95,7 +127,7 @@ export class UntypedWhether extends Function {
 
   /**
    * Tell whether the target is null or undefined
-   * @param o
+   * @param o target
    */
   isNullish(o: any): o is null | undefined {
     return o === null || o === undefined;
@@ -103,7 +135,7 @@ export class UntypedWhether extends Function {
 
   /**
    * Tell whether the target is a primitive value
-   * @param o
+   * @param o target
    */
   isPrimitive(o: any): o is string | number | boolean | null | undefined | symbol | bigint {
     const t = typeof o;
@@ -118,14 +150,28 @@ export class UntypedWhether extends Function {
     );
   }
 
+  /**
+   * Tell whether the target is a field key (string or symbol)
+   * @param o target
+   */
   isField(o: unknown): o is string | symbol {
     return typeof o === 'string' || typeof o === 'symbol';
   }
 
+  /**
+   * Tell whether the target is a property key (string, symbol or number)
+   * @param o target
+   */
   isPropertyKey(o: unknown): o is string | symbol | number {
     return typeof o === 'string' || typeof o === 'symbol' || typeof o === 'number';
   }
 
+  /**
+   * Tell whether the target is NaN
+   * - if target is a number, it will return `true` if it is `NaN`
+   * - if target is not a number, it will return `null`
+   * @param o target
+   */
   isNaN(o: any): boolean | null {
     if (typeof o !== 'number') {
       return null;
@@ -134,18 +180,34 @@ export class UntypedWhether extends Function {
     return Number.isNaN(o);
   }
 
+  /**
+   * Tell whether the target is an integer
+   * @param o target
+   */
   isInteger(o: any): boolean {
     return Number.isInteger(o);
   }
 
+  /**
+   * Tell whether the target is a safe integer
+   * @param o target
+   */
   isSafeInteger(o: any): boolean {
     return Number.isSafeInteger(o);
   }
 
+  /**
+   * Tell whether the target is a safe number
+   * @param o target
+   */
   isSafeNumber(o: any): boolean {
     return typeof o === 'number' && o <= Number.MAX_SAFE_INTEGER && o >= Number.MIN_SAFE_INTEGER;
   }
 
+  /**
+   * Tell whether the target is a class constructor
+   * @param o target
+   */
   isClass(o: any): o is Class {
     if (typeof o !== 'function') {
       return false;
@@ -223,114 +285,163 @@ export class UntypedWhether extends Function {
   // #endregion
 
   // #region Can be undefined
+  /**
+   * (Optional) Tell whether the target is negative zero
+   * @param o target
+   */
   orNegativeZero(o: any): boolean {
     return o === undefined || this.isNegativeZero(o);
   }
 
+  /**
+   * (Optional) Tell whether the target is positive zero
+   * @param o target
+   */
   orPositiveZero(o: any): boolean {
     return o === undefined || this.isPositiveZero(o);
   }
 
+  /**
+   * (Optional) Tell whether the target is a function
+   * @param o target
+   */
   orFunction(o: any): o is Func | undefined {
     return o === undefined || this.isFunction(o);
   }
 
+  /**
+   * (Optional) Tell whether the target is a non-null object
+   * @param o target
+   */
   orObject<T extends object>(o: any): o is T | undefined {
     return o === undefined || this.isObject(o);
   }
 
   /**
-   * Tell whether the target is a non-null object or a function
-   * @param o
+   * (Optional) Tell whether the target is a non-null object or a function
+   * @param o target
    */
   orLikeObject(o: unknown) {
     return o === undefined || this.likeObject(o);
   }
 
   /**
-   * Tell whether the target is a string
-   * @param o
+   * (Optional) Tell whether the target is a string
+   * @param o target
    */
   orString(o: any): o is string | undefined {
     return o === undefined || this.isString(o);
   }
 
   /**
-   * Tell whether the target is a number
-   * @param o
+   * (Optional) Tell whether the target is a number
+   * @param o target
    */
   orNumber(o: any): o is number | undefined {
     return o === undefined || this.isNumber(o);
   }
 
   /**
-   * Tell whether the target is a boolean
-   * @param o
+   * (Optional) Tell whether the target is a boolean
+   * @param o target
    */
   orBoolean(o: any): o is boolean | undefined {
     return o === undefined || this.isBoolean(o);
   }
 
   /**
-   * Tell whether the target is undefined
-   * @param o
+   * (Optional) Tell whether the target is undefined
+   * @param o target
    */
   orUndefined(o: any): o is undefined {
     return o === undefined || this.isUndefined(o);
   }
 
   /**
-   * Tell whether the target is null
-   * @param o
+   * (Optional) Tell whether the target is null
+   * @param o target
    */
   orNull(o: any): o is null | undefined {
     return o === undefined || this.isNull(o);
   }
 
   /**
-   * Tell whether the target is a symbol
-   * @param o
+   * (Optional) Tell whether the target is a symbol
+   * @param o target
    */
   orSymbol(o: any): o is symbol | undefined {
     return o === undefined || this.isSymbol(o);
   }
 
   /**
-   * Tell whether the target is a bigint
-   * @param o
+   * (Optional) Tell whether the target is a bigint
+   * @param o target
    */
   orBigInt(o: any): o is bigint | undefined {
     return o === undefined || this.isBigInt(o);
   }
 
+  /**
+   * (Optional) Tell whether the target is a field key (string or symbol)
+   * @param o target
+   */
   orField(o: unknown): o is string | symbol | undefined {
     return o === undefined || this.isField(o);
   }
 
+  /**
+   * (Optional) Tell whether the target is a property key (string, symbol or number)
+   * @param o target
+   */
   orPropertyKey(o: unknown): o is string | symbol | number | undefined {
     return o === undefined || this.isPropertyKey(o);
   }
 
+  /**
+   * (Optional) Tell whether the target is NaN
+   * @param o target
+   */
   orNaN(o: any): boolean | null {
     return o === undefined || this.isNaN(o);
   }
 
+  /**
+   * (Optional) Tell whether the target is an integer
+   * @param o target
+   */
   orInteger(o: any): boolean {
     return o === undefined || this.isInteger(o);
   }
 
+  /**
+   * (Optional) Tell whether the target is a safe integer
+   * @param o target
+   */
   orSafeInteger(o: any): boolean {
     return o === undefined || this.isSafeInteger(o);
   }
 
+  /**
+   * (Optional) Tell whether the target is a safe number
+   * @param o target
+   */
   orSafeNumber(o: any): boolean {
     return o === undefined || this.isSafeNumber(o);
   }
 
+  /**
+   * (Optional) Tell whether the target is a class constructor
+   * @param o target
+   */
   orClass(o: any): o is Class | undefined {
     return o === undefined || this.isClass(o);
   }
 
+  /**
+   * (Optional) Tell whether the target is an array
+   * @param o target array
+   * @param predicate function to validate each element
+   */
   orArray<T = any>(
     o: any,
     predicate: (value?: T, index?: number, array?: T[]) => string | boolean = NOT_GIVEN as any
